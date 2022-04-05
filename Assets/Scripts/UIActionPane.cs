fd78901a5dfd05a823477d989eb6768047bd4e54
seylorx1 <61364803+seylorx1@users.noreply.github.com>
2022-04-05 15:16:07 +0100

    Added text and animations

--- Assets/Scripts/UIActionPane.cs
+++ Assets/Scripts/UIActionPane.cs
@@ -9,39 +9,53 @@ public class UIActionPane : MonoBehaviour
     public float colorTransitionSpeed = 2.0f;
     public Color shapeBG, patternBG, colorBG;
 
-    private Image image;
-    private Color startColor;
-    private Color targetColor;
+    public Animator headingAnimator;
+
+    private Image _image;
+    private Color _startColor;
+    private Color _targetColor;
+
+    private string _lastScreen = "";
     // Start is called before the first frame update
     void Start()
     {
-        image = GetComponent<Image>();
-        startColor = image.color;
-        targetColor = startColor;
+        _image = GetComponent<Image>();
+        _startColor = _image.color;
+        _targetColor = _startColor;
     }
 
     // Update is called once per frame
     void Update()
     {
-        image.color = Color.Lerp(image.color, targetColor, Time.deltaTime * colorTransitionSpeed);
+        _image.color = Color.Lerp(_image.color, _targetColor, Time.deltaTime * colorTransitionSpeed);
     }
 
     public void ChangeScreen(string screen)
     {
+        if(screen == _lastScreen)
+        {
+            //Same screen has been pressed.. close.
+            screen = "";
+        }
+
+        headingAnimator.SetBool("Show", false);
         switch (screen)
         {
             case "Shape":
-                targetColor = shapeBG;
+                _targetColor = shapeBG;
                 break;
             case "Pattern":
-                targetColor = patternBG;
+                _targetColor = patternBG;
                 break;
             case "Color":
-                targetColor = colorBG;
+                _targetColor = colorBG;
                 break;
             default:
-                targetColor = startColor;
+                _targetColor = _startColor;
+                headingAnimator.SetBool("Show", true);
                 break;
         }
+
+        _lastScreen = screen;
     }
 }
