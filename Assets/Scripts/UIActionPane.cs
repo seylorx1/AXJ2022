354904093933e96dd4f6f611ae8c76b2bb7f4af6
seylorx1 <61364803+seylorx1@users.noreply.github.com>
2022-04-05 17:41:28 +0100

    Shape masks created and added to project files.

--- Assets/Scripts/UIActionPane.cs
+++ Assets/Scripts/UIActionPane.cs
@@ -5,23 +5,35 @@ using UnityEngine.UI;
 
 public class UIActionPane : MonoBehaviour
 {
+    private enum SelectedPage
+    {
+        None,
+        Shape,
+        Pattern,
+        Color
+    }
 
     public float colorTransitionSpeed = 2.0f;
-    public Color shapeBG, patternBG, colorBG;
+    public Button shapeButton, patternButton, colorButton;
 
     public Animator headingAnimator;
+
+    private SelectedPage _currentPage;
 
     private Image _image;
     private Color _startColor;
     private Color _targetColor;
 
-    private string _lastScreen = "";
     // Start is called before the first frame update
     void Start()
     {
         _image = GetComponent<Image>();
         _startColor = _image.color;
         _targetColor = _startColor;
+
+        shapeButton     .onClick.AddListener(() => ChangeScreen(SelectedPage.Shape));
+        patternButton   .onClick.AddListener(() => ChangeScreen(SelectedPage.Pattern));
+        colorButton     .onClick.AddListener(() => ChangeScreen(SelectedPage.Color));
     }
 
     // Update is called once per frame
@@ -30,32 +42,43 @@ public class UIActionPane : MonoBehaviour
         _image.color = Color.Lerp(_image.color, _targetColor, Time.deltaTime * colorTransitionSpeed);
     }
 
-    public void ChangeScreen(string screen)
+    private void ChangeScreen(SelectedPage page)
     {
-        if(screen == _lastScreen)
+        //Go back to main page if a button is selected again.
+        if(page == _currentPage)
         {
-            //Same screen has been pressed.. close.
-            screen = "";
+            //Deselect page enum.
+            page = SelectedPage.None;
+
+            //Make sure that no button is selected.
+            GameObject selected = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+            if(selected == shapeButton.gameObject || selected == patternButton.gameObject || selected == colorButton.gameObject)
+            {
+                UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
+            }
         }
 
-        headingAnimator.SetBool("Show", false);
-        switch (screen)
+        //Show / Hide heading based on whether the base page is selected or not.
+        headingAnimator.SetBool("Show", page == SelectedPage.None);
+
+        //Update colors
+        switch (page)
         {
-            case "Shape":
-                _targetColor = shapeBG;
+            case SelectedPage.Shape:
+                _targetColor = shapeButton.colors.selectedColor;
                 break;
-            case "Pattern":
-                _targetColor = patternBG;
+            case SelectedPage.Pattern:
+                _targetColor = patternButton.colors.selectedColor;
                 break;
-            case "Color":
-                _targetColor = colorBG;
+            case SelectedPage.Color:
+                _targetColor = colorButton.colors.selectedColor;
                 break;
             default:
                 _targetColor = _startColor;
-                headingAnimator.SetBool("Show", true);
                 break;
         }
 
-        _lastScreen = screen;
+        //Update current page reference.
+        _currentPage = page;
     }
 }
